a5da4bd7a79904862909ebea3d8ff54c7bddd22e
Naiabunner <Okechukwudaniel12@gmail.com>
2023-12-07 20:54:39 +0000

    update Home project 01

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import Hero from "./hero";
+import HomeProjects from "./homeprojects";
 import Quote from "./quote";
 
 const Home = () => {
     return ( <div className="Home">
         <Hero/>
         <Quote/>
+        <HomeProjects/>
     </div> );
 }
  
---
+++ src/pages/Home/homeprojects.jsx
@@ -0,0 +1,19 @@
+const HomeProjects = () => {
+    return ( <>
+    <div className="homeProjects_wrapper">
+        <div className="homeProjects_head">
+            <div className="homeProjects_head_one">
+                <h2><span>#</span>projects </h2>
+                <p id="line_design"></p>
+            </div>
+
+            <div className="homeProjects_head_two">
+            <p>View all {"~~>"}</p>
+
+            </div>
+        </div>
+    </div>
+    </> );
+}
+ 
+export default HomeProjects;
