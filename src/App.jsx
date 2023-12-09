35b982e76a62b89154378437be43b0a4b770c7c0
Naiabunner <Okechukwudaniel12@gmail.com>
2023-12-09 19:39:10 +0000

    fixed footer deploy issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import Footer from "./components/Footer";
+import Footer from "./components/footer";
 import Navbar from "./components/Navbar";
 import Routepages from "./react-router/app_router";
 function App() {
