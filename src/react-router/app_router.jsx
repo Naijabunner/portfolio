576fc0b9b004ae5cf9430685511e893f2f1636ff
Naiabunner <Okechukwudaniel12@gmail.com>
2023-12-09 20:20:03 +0000

    Homepage

--- src/react-router/app_router.jsx
+++ src/react-router/app_router.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Homepage from "../pages/Home/home";
+import Homepage from "../pages/Home/Homepage";
 const Routepages = () => {
     return (<>
     
