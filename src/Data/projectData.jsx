5fe6c29c6cc61ef33114f4bf5b8e92f37aa55aa8
Naiabunner <Okechukwudaniel12@gmail.com>
2023-12-24 14:08:27 +0000

    Updated home projects

---
+++ src/Data/projectData.jsx
@@ -0,0 +1,31 @@
+import Img from "/src/assets/portfolio.png";
+const UseProjectData = () => {
+    const projects =[
+        {
+        img: Img,
+        tools:"HTML CSS3 REACTJS NETLIFY",
+        project_name:"Portfolio",
+        notes:"You're currently using it"
+    },
+        {
+        img: Img,
+        tools:"HTML CSS3 REACTJS NETLIFY",
+        project_name:"Portfolio",
+        notes:"You're currently using it"
+    },
+        {
+        img: Img,
+        tools:"HTML CSS3 REACTJS NETLIFY",
+        project_name:"Portfolio",
+        notes:"You're currently using it"
+    },
+        {
+        img: Img,
+        tools:"HTML CSS3 REACTJS NETLIFY",
+        project_name:"Portfolio",
+        notes:"You're currently using it"
+    },]
+    return ( projects );
+}
+ 
+export default UseProjectData;
--- src/pages/Home/homeprojects.jsx
+++ src/pages/Home/homeprojects.jsx
@@ -1,43 +1,45 @@
+import UseProjectData from "../../Data/projectData";
 const HomeProjects = () => {
-    return ( <>
-    <div className="homeProjects_wrapper">
-        <div className="homeProjects_head">
-            <div className="homeProjects_head_one">
-                <h2><span>#</span>projects </h2>
-                <p id="line_design"></p>
-            </div>
-                <div className="homeProjects_head_two">
-                            <p>View all {"~~>"}</p>
-
-                            </div>
-           
+  const data = UseProjectData();
+  const mappedProjects = data.map((project) => {
+    return (
+      <div className="d">
+        <div className="project_img">
+          <img src={project.img} alt="" />
         </div>
-         <div className="dd">
-             <div className="d">
-                <div className="project_img"></div>
-                <div className="project_tools"></div>
-                <div className="project_innerWrapper">
-                    <h3 className="project_name"></h3>
-                    <p></p>
-                    <div className="btns_wrapper">
-                        <button className="github_btn"></button>
-                        <button className="live_btn"></button>
-                    </div>
-                </div>
-                qwertyuioccvbnm,vbnvbnm vbnmcvbnmcvbnmcvbnmvbnvbnmv bnvbnbnm
-            </div>
-            <div className="d">
-            qwertyuioccvbnm,vbnvbnm vbnmcvbnmcvbnmcvbnmvbnvbnmv bnvbnbnm
+        <div className="project_tools">
+            <p>{project.tools}</p>
+        </div>
+        <div className="project_innerWrapper">
+            <h3 className="project_name">{project.project_name}</h3>
+          <p>{project.notes}</p>
+          <div className="btns_wrapper">
+            <button className="github_btn">GitHub {"<~~>"}</button>
+            <button className="live_btn">Live {"<~~>"}</button>
+          </div>
+        </div>
+      </div>
+    );
+  });
 
-            </div>
-            <div className="d">
-            qwertyuioccvbnm,vbnvbnm vbnmcvbnmcvbnmcvbnmvbnvbnmv bnvbnbnm
+  return (
+    <>
+      <div className="homeProjects_wrapper">
+        <div className="homeProjects_head">
+          <div className="homeProjects_head_one">
+            <h2>
+              <span>#</span>projects{" "}
+            </h2>
+            <p id="line_design"></p>
+          </div>
+          <div className="homeProjects_head_two">
+            <p>View all {"~~>"}</p>
+          </div>
+        </div>
+        <div className="dd">{mappedProjects}</div>
+      </div>
+    </>
+  );
+};
 
-            </div>
-         </div>
-           
-    </div>
-    </> );
-}
- 
 export default HomeProjects;
